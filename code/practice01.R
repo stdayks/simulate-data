440077349e2a06d4039fe810f4bfb33002b8a66a
stdayks <std.ayks@gmail.com>
2023-08-13 02:38:36 +0900

    2nd commit_practice_calculate or so

---
+++ code/practice01.R
@@ -0,0 +1,46 @@
+3+3
+
+8*2
+
+3>2
+2 + 1 == 3
+
+2 + 3 == 1
+
+(2 + 3 == 5) | (1 * 2 == 3)
+
+123454321 * 2
+
+x <- 123454321
+
+x
+
+print(x)
+
+(y <- 2)
+
+x*2
+
+x*3
+
+x * 4
+
+x <- "猫の恋 やむとき閨の 朧月（芭蕉）"
+
+x
+
+(numeric_vec1  <- c(73, 6, 5, 3, 99, 10, 22, 9, 7))
+
+numeric_vec1
+
+character_vec <- c('cat',  'cheetah',　 'lion',  'tiger')
+
+character_vec
+
+(numeric_vec2 <- seq(from = 20, to = 1, by = -5))
+
+21:30
+
+help("[") 
+
+numeric_vec2[3]
